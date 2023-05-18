d0621d659db394a1514b761233a146c7895bc03c
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-18 16:48:41 +0500

    Delete Авторизация directory

--- Авторизация/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    const bttn = document.getElementById('enter-button');
-    if (bttn) {
-        bttn.addEventListener('click', () => {
-            console.log('Button pressed')
-        });
-    }
-});
