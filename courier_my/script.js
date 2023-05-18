ca1265569b84234b0c5a537693421eb7d9603c78
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-18 23:37:19 +0500

    popup

--- courier_my/script.js
+++ courier_my/script.js
@@ -1,3 +1,5 @@
+document.getElementById('popup').hidden = true;
+
 window.addEventListener("DOMContentLoaded", (event) => {
     const bttn = document.getElementById('change-password-btn');
     if (bttn) {
