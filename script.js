60150d1f7f956804a3753331e1b1aed928525726
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-19 22:28:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 window.addEventListener("DOMContentLoaded", (event) => {
     const bttn = document.getElementById('enter-button');
     if (bttn) {
@@ -6,4 +7,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
             console.log('Button pressed')
         });
     }
+});
+
+onst data = {
+  // Данные для отправки на сервер
+};
+
+$.ajax({
+  type: 'POST',
+  url: 'https://localhost:3000/send-data', // Адрес сервера
+  data: JSON.stringify({ data }), // Преобразуем данные в JSON и отправляем на сервер
+  contentType: 'application/json',
+  success: (response) => {
+    console.log(response); // Получаем ответ от сервера
+  },
+  error: (error) => {
+    console.log(error);
+  }
 });
