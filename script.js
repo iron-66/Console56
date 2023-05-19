fabf648036ccadf98b3c2cce1fc417b960ca631a
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-19 23:11:09 +0500

    js edit

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    const bttn = document.getElementById('enter-button');
-    if (bttn) {
-        bttn.addEventListener('click', () => {
-            console.log('Button pressed')
-        });
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const enterButton = document.getElementById("enter-button");
+
+  enterButton.addEventListener("click", () => {
+    const loginInput = document.querySelector(".login-input");
+    const passwordInput = document.querySelector(".password-input");
+
+    const login = loginInput.value;
+    const password = passwordInput.value;
+
+    // Отправка данных на локальный компьютер
+    sendLoginAndPassword(login, password);
+  });
 });
 
-const data = {
-  // Данные для отправки на сервер
-};
-
-$.ajax({
-  type: 'POST',
-  url: 'https://localhost:3000/send-data', // Адрес сервера
-  data: JSON.stringify({ data }), // Преобразуем данные в JSON и отправляем на сервер
-  contentType: 'application/json',
-  success: (response) => {
-    console.log(response); // Получаем ответ от сервера
-  },
-  error: (error) => {
-    console.log(error);
-  }
-});
+function sendLoginAndPassword(login, password) {
+  fetch("https://localhost:3000/check-login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      login: login,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Проверка ответа от локального компьютера
+      if (data.valid) {
+        // Перенаправление пользователя на указанную страницу
+        window.location.href = "https://iron-66.github.io/Console56/manager_my/";
+      } else {
+        // Обработка случая некорректных данных для входа
+        alert("Некорректные данные для входа");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      // Обработка ошибки при отправке запроса
+      alert("Произошла ошибка при отправке данных");
+    });
+}
