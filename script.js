37099a3d31508c6d5a3d769b9e00a6ca69092e49
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-19 22:24:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 window.addEventListener("DOMContentLoaded", (event) => {
     const bttn = document.getElementById('enter-button');
     if (bttn) {
