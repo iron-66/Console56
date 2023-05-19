1900b579a353f5a9e2ab7ee634738207bb49678d
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-20 00:31:56 +0500

    Edit JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+{/* <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> */}
 
 document.addEventListener("DOMContentLoaded", () => {
   const enterButton = document.getElementById("enter-button");
