964b88a4078558a3a76712f562a269d397af4f0a
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-24 00:40:34 +0500

    Choose job func

--- registration/script.js
+++ registration/script.js
@@ -3,51 +3,62 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Возврат в личный кабинет
-    const backBtn = document.getElementById('back-btn');
-    backBtn.addEventListener('click', () => {
-      const redirectUrl = `https://iron-66.github.io/Console56/manager_my/?id=${id}`;
-      window.location.href = redirectUrl;
-    });
+  // Возврат в личный кабинет
+  const backBtn = document.getElementById('back-btn');
+  backBtn.addEventListener('click', () => {
+    const redirectUrl = `https://iron-66.github.io/Console56/manager_my/?id=${id}`;
+    window.location.href = redirectUrl;
+  });
 
-    // Отправка данных о новом сотруднике
-    const enterButton = document.getElementById('add-button');
-    enterButton.addEventListener('click', () => {
-  
-      const loginInput = document.getElementById('login-input');
-      const passwordInput = document.getElementById('password-input');
+  // Отправка данных о новом сотруднике
+  const enterButton = document.getElementById('add-button');
+  enterButton.addEventListener('click', () => {
 
-  
-      const login = loginInput.value;
-      const password = passwordInput.value;
-  
-      if (loginInput.value === '' || passwordInput.value === '') {
-        alert('Заполните все поля');
-      }
-      else {
-        sendLoginAndPassword(login, password);
-      }
+    const loginInput = document.getElementById('login-input');
+    const passwordInput = document.getElementById('password-input');
+
+
+    const login = loginInput.value;
+    const password = passwordInput.value;
+
+    if (loginInput.value === '' || passwordInput.value === '') {
+      alert('Заполните все поля');
+    }
+    else {
+      sendLoginAndPassword(login, password);
+    }
+  });
+
+  // Выбор должности сотрудника
+  const jobItems = document.querySelectorAll('.job-item');
+  jobItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      jobItems.forEach((item) => {
+        item.classList.remove('active');
+      });
+      event.currentTarget.classList.add('active');
     });
   });
+});
   
-  // Отправка данных на локальный компьютер
-  function sendLoginAndPassword(login, password) {
-    fetch('http://localhost:3000/add-user', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        login: login,
-        password: password,
-      }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        alert('Пользователь успешно зарегистрирован');
-    })
-    .catch((error) => {
-      console.log(error);
-      alert('Произошла ошибка при отправке данных');
-    });
-  }
+// Отправка данных на локальный компьютер
+function sendLoginAndPassword(login, password) {
+  fetch('http://localhost:3000/add-user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      login: login,
+      password: password,
+    }),
+  })
+  .then((response) => response.json())
+  .then((data) => {
+      alert('Пользователь успешно зарегистрирован');
+  })
+  .catch((error) => {
+    console.log(error);
+    alert('Произошла ошибка при отправке данных');
+  });
+}
