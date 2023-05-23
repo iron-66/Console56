0c53c33ab02193f62ebd9705ed971c43d2a989aa
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-23 12:31:02 +0500

    Added ID url forr buttons

--- manager_my/script.js
+++ manager_my/script.js
@@ -72,4 +72,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
         const redirectUrl = `https://iron-66.github.io/Console56/registration/?id=${id}`;
         window.location.href = redirectUrl;
     });
+
+    // Переход в актуальные заказы
+    const actualOrd = document.getElementById('actual-orders');
+    actualOrd.addEventListener('click', () => {
+        const redirectUrl = `https://iron-66.github.io/Console56/manager_active/?id=${id}`;
+        window.location.href = redirectUrl;
+    });
+
+    // Переход в окно добавления заказа
+    const newOrd = document.getElementById('new-order');
+    newOrd.addEventListener('click', () => {
+        const redirectUrl = `https://iron-66.github.io/Console56/manager_new/?id=${id}`;
+        window.location.href = redirectUrl;
+    });
+
+    // Переход в архив заказов
+    const archiveOrd = document.getElementById('orders-archive');
+    archiveOrd.addEventListener('click', () => {
+        const redirectUrl = `https://iron-66.github.io/Console56/manager_archive/?id=${id}`;
+        window.location.href = redirectUrl;
+    });
 });
