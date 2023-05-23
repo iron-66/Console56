08656e8614f40c8ca9d6fdb1633d82938970e2e4
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-23 10:51:54 +0500

    Password change

--- manager_my/script.js
+++ manager_my/script.js
@@ -1,6 +1,7 @@
 // Получение значения параметра id из URL
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
+console.log(id);
 
 fetch('http://localhost:3000/receipt-for-manger', {
     method: 'POST',
@@ -16,17 +17,55 @@ fetch('http://localhost:3000/receipt-for-manger', {
 console.log(id);
 
 window.addEventListener("DOMContentLoaded", (event) => {
+    // Открытие попапа смены пароля
     const add_btn = document.getElementById('change-password-btn');
-    if (add_btn) {
-        add_btn.addEventListener('click', () => {
-            document.getElementById('popup').hidden = false;
-        });
+    add_btn.addEventListener('click', () => {
+        document.getElementById('popup').hidden = false;
+    });
+
+    // Выход из попапа смены пароля
+    const back_btn = document.getElementById('back_btn');
+    back_btn.addEventListener('click', () => {
+        document.getElementById('popup').hidden = true;
+    });
+
+    // Смена пароля
+    const oldPassInput = document.getElementById('old-pass');
+    const newPassInput = document.getElementById('new-pass');
+    const confirmPassInput = document.getElementById('new-pass-confirm');
+    const changePassButton = document.getElementById('change-pass-button');
+
+    changePassButton.addEventListener('click', () => {
+    const oldPass = oldPassInput.value;
+    const newPass = newPassInput.value;
+    const confirmPass = confirmPassInput.value;
+
+    if (newPass !== confirmPass) {
+        alert('Новые пароли не совпадают');
+        return;
     }
 
-    const back_btn = document.getElementById('back_btn');
-    if (back_btn) {
-        back_btn.addEventListener('click', () => {
-            document.getElementById('popup').hidden = true;
+    fetch('http://localhost:3000/change-password', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id,
+            oldPass: oldPass,
+            newPass: newPass,
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+        if (data.success) {
+            alert('Пароль успешно изменен');
+        } else {
+            alert('Не удалось изменить пароль');
+        }
+        })
+        .catch(error => {
+        console.error('Ошибка при выполнении запроса:', error);
         });
-    }
+    });
 });
