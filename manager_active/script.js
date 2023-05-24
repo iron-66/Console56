b02d790e771d48f657fabd2adf5d55b2ddf1cc55
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-24 20:51:23 +0500

    JS

--- manager_active/script.js
+++ manager_active/script.js
@@ -16,7 +16,52 @@ async function getManagerOrders() {
         
         const list = document.getElementById('act-orders-list');
         orders.forEach(order => {
-            const newLiHTML = "<li>Новый элемент</li>";
+            const newLiHTML = `
+            <li class="actual-orders-item">
+                    <div class="about-order">
+                        <div class="order-item-number">
+                            <p><b>Заказ от:</b></p>
+                            <p id="createdate" class="order-number"></p>
+                        </div>
+                        <div class="order-item-status">
+                            <p><b>Статус заказа:</b></p>
+                            <p id="orderstatus" class="">Новый</p>
+                        </div>
+                    </div>
+                    <div class="about-customer">
+                        <div class="order-item-customer">
+                            <p>Заказчик:</p>
+                            <p id="name" class="order-customer"></p>
+                        </div>
+                        <div class="order-item-address">
+                            <p>Адрес:</p>
+                            <p id="address" class="order-address"></p>
+                        </div>
+                        <div class="order-item-phone-number">
+                            <p>Телефон:</p>
+                            <p id="phone" class="order-phone-number"></p>
+                        </div>
+                    </div>
+                    <div class="buttons">
+                        <button class="more-info-btn"></button>
+                        <button class="cancel-btn"></button>
+                        <button class="edit-btn"></button>
+                    </div>
+                    <div class="order-content">
+                        <div class="order-content-header">
+                            <p class="order-content-header-cntnt">Содержимое заказа:</p>
+                            <p class="order-content-header-amount">Кол-во:</p>
+                            <p lass="order-content-header-cost">Стоимость:</p>
+                        </div>
+                        <ul class="order-content-list">
+                            <li class="order-content-list-item">
+                                <p id="products" class="order-content-item-name""</p>
+                                <p id="amounts" class="order-content-item-amount">2</p>
+                                <p id="cost" class="order-content-item-cost"></p>
+                            </li>
+                        </ul>
+                    </div>
+                </li>`;
             list.insertAdjacentHTML("beforeend", newLiHTML);
         });
     } catch (error) {
