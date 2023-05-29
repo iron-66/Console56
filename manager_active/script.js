3fa146a3bfbdb997fc22710851c6aa6a6e3c2001
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-29 23:37:23 +0500

    userid edit

--- manager_active/script.js
+++ manager_active/script.js
@@ -97,7 +97,7 @@ function getUserData(user) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        user: user.toString(),
+        user: user,
       }),
     })
     .then((response) => response.json())
