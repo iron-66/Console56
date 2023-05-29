96b816485f351a0c94f3e4ea05690660c9734e94
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-29 21:11:17 +0500

    edit datetime

--- manager_new/script.js
+++ manager_new/script.js
@@ -104,9 +104,9 @@ function formatToday(dateString) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const hours = currentDate.getHours().toString().padStart(2, '0');
-    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
     
     return `'${year}-${month}-${day} ${hours}:${minutes}'`;
 };
