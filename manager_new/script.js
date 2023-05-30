040df100532390af6b8f2742c6175ccb6b5cf94f
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-30 19:44:52 +0500

    JS

--- manager_new/script.js
+++ manager_new/script.js
@@ -99,17 +99,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-function formatToday(dateString) {
-    const date = new Date(dateString);
+function formatToday(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
     
-    return `'${year}-${month}-${day} ${hours}:${minutes}'`;
-};
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
 
 // Отправка данных на локальный компьютер
 function setNewOrder(name, address, phone, today, orderItems) {
