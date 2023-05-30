666972722fbd70c8fb1cd5f389c6201a30ce02ad
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-30 11:53:39 +0500

    Edit

--- manager_active/script.js
+++ manager_active/script.js
@@ -101,6 +101,7 @@ function getUserData(userid) {
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
+        return data;
     })
     .catch((error) => {
       console.log(error);
