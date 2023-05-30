8cf4b48b5ce4c06b99a5fbfd634483258a065dfe
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-30 12:06:03 +0500

    edit

--- manager_active/script.js
+++ manager_active/script.js
@@ -15,6 +15,7 @@ async function getManagerOrders() {
         const list = document.getElementById('act-orders-list');
         for (const order of orders) {
             const userResponse = getUserData(order.userid);
+            console.log(userResponse.name)
             //const userData = userResponse.json();
             const newLiHTML = `
             <li class="actual-orders-item">
