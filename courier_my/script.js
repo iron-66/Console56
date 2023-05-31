927d29c3d4d5bb0eee283b8ce2a58146573eb6f6
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-31 11:05:30 +0500

    Links edit

--- courier_my/script.js
+++ courier_my/script.js
@@ -12,4 +12,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
             document.getElementById('popup').hidden = true;
         });
     }
+
+    // Переход в выбор заказов
+    const actualOrd = document.getElementById('actual-orders-button');
+    actualOrd.addEventListener('click', () => {
+        const redirectUrl = `https://iron-66.github.io/Console56/courier_choose/?id=${id}`;
+        window.location.href = redirectUrl;
+    });
+
+    // Переход в принятые заказы
+    const newOrd = document.getElementById('accepted-orders-button');
+    newOrd.addEventListener('click', () => {
+        const redirectUrl = `https://iron-66.github.io/Console56/courier_active/?id=${id}`;
+        window.location.href = redirectUrl;
+    });
+
+    // Переход в архив заказов
+    const newOrd = document.getElementById('orders-archive-button');
+    newOrd.addEventListener('click', () => {
+        const redirectUrl = `https://iron-66.github.io/Console56/courier_archive/?id=${id}`;
+        window.location.href = redirectUrl;
+    });
 });
