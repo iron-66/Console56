130f04f121a0f0663016650a86721793c5539ae3
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-05-31 15:15:42 +0500

    New datetime format

--- manager_new/script.js
+++ manager_new/script.js
@@ -107,7 +107,7 @@ function formatToday(date) {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
   
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.000000`;
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 // Отправка данных на локальный компьютер
