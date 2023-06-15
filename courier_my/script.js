441b4560839556bbecfe96406b67a73ac9772631
iron-66 <61771808+iron-66@users.noreply.github.com>
2023-06-15 13:08:32 +0500

    Receipts edit

--- courier_my/script.js
+++ courier_my/script.js
@@ -10,49 +10,48 @@ if (id === null) {
 // Функция для получения квитанций от сервера
 async function getReceipts() {
     try {
-      const response = await fetch(`http://localhost:3000/check-courier-receipts?employeeid=${id}`);
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      const receipts = await response.json();
-      const fullname = receipts.fullname;
-      const monthlyShifts = receipts.monthlyShifts;
-      console.log(fullname, monthlyShifts);
-  
-      const list = document.getElementById('receipts');
-      for (const [monthYear, shifts] of Object.entries(monthlyShifts)) {
-  
-        const newLiHTML = `
-        <li class="receipts-item">
-            <div class="about-receipt">
-                <div class="receipts-item-date-and-time">
-                    <p><b>Квитанция за:</b></p>
-                    <p id="date" class="receipts-date-and-time">${monthYear}</p>
+        const response = await fetch(`http://localhost:3000/check-courier-receipts?employeeid=${id}`);
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        const receipts = await response.json();
+        const fullname = receipts.fullname;
+        const monthlyShifts = receipts.monthlyShifts;
+        console.log(fullname, monthlyShifts);
+    
+        const list = document.getElementById('receipts');
+        for (const [monthYear, count] of Object.entries(monthlyCounts)) {
+            const newLiHTML = `
+            <li class="receipts-item">
+                <div class="about-receipt">
+                    <div class="receipts-item-date-and-time">
+                        <p><b>Квитанция за:</b></p>
+                        <p id="date" class="receipts-date-and-time">${monthYear}</p>
+                    </div>
+                    <div class="receipts-item-employee">
+                        <p>Сотрудник:</p>
+                        <p id="employee" class="employee">${fullname}</p>
+                    </div>
+                    <div class="receipts-item-sum">
+                        <p>Количество выполненных заказов:</p>
+                        <p id="count" class="count">${count}</p>
+                    </div>
+                    <div class="receipts-item-taxes">
+                        <p>Размер ставки:</p>
+                        <p id="bid" class="bid">2000 руб.</p>
+                    </div>
+                    <div class="receipts-item-fees">
+                        <p>Без учёта налогового вычета:</p>
+                        <p id="taxless" class="taxless">${count * 2000} руб.</p>
+                    </div>
+                    <div class="receipts-item-total">
+                        <p><b>Итого:</b></p>
+                        <p id="total" class="total">${count * 2000 * 0.87} руб.</p>
+                    </div>
                 </div>
-                <div class="receipts-item-employee">
-                    <p>Сотрудник:</p>
-                    <p id="employee" class="employee">${fullname}</p>
-                </div>
-                <div class="receipts-item-sum">
-                    <p>Количество смен:</p>
-                    <p id="shifts" class="shifts">${shifts}</p>
-                </div>
-                <div class="receipts-item-taxes">
-                    <p>Размер ставки:</p>
-                    <p id="bid" class="bid">2000 руб.</p>
-                </div>
-                <div class="receipts-item-fees">
-                    <p>Без учёта налогового вычета:</p>
-                    <p id="taxless" class="taxless">${shifts * 2000} руб.</p>
-                </div>
-                <div class="receipts-item-total">
-                    <p><b>Итого:</b></p>
-                    <p id="total" class="total">${shifts * 2000 * 0.87} руб.</p>
-                </div>
-            </div>
-        </li>`;
-        list.insertAdjacentHTML("afterbegin", newLiHTML);
-      };
+            </li>`;
+            list.insertAdjacentHTML("afterbegin", newLiHTML);
+        };
     } catch (error) {
       console.error('Error:', error);
     }
